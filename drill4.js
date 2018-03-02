d19a14ce6941ddf4226a7bf85431f90a84641282
jirist <jiristanc@gmail.com>
2018-03-02 09:49:54 +0000

    creates display2 function

--- drill4.js
+++ drill4.js
@@ -24,3 +24,12 @@ function display(event) {
 //attach event listener to h3 elements to invoke display function when clicked
 
 $("h3").click(display);
+
+
+//displays and animates the next element after the current target
+
+function display2(event) {
+    
+	$(event.currentTarget).next().animate( {height: ‘toggle’}, 'slow');
+	
+} //end of display2
