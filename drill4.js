3d7da9546c6c194e969838061ecb866a361535eb
jirist <jiristanc@gmail.com>
2018-03-02 09:13:54 +0000

    adds new file

---
+++ drill4.js
@@ -0,0 +1,26 @@
+/*
+
+Program Name:   Recipe Display Application
+
+Author:         Jiri Stanc
+
+Date:           March 1, 2018
+
+Filename:       script.js
+
+*/
+
+
+
+//displays the next element after the current target
+
+function display(event) {
+    
+    $(event.currentTarget).next().fadeIn('slow');
+    
+} //end of display
+
+
+//attach event listener to h3 elements to invoke display function when clicked
+
+$("h3").click(display);
