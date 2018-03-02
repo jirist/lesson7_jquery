b5d9cad58aa7cf1a4958d839b33a560146f5f9d5
jirist <jiristanc@gmail.com>
2018-03-02 09:56:42 +0000

    creates new h3 click event

--- drill4.js
+++ drill4.js
@@ -33,3 +33,8 @@ function display2(event) {
 	$(event.currentTarget).next().animate( {height: ‘toggle’}, 'slow');
 	
 } //end of display2
+
+
+//attach event listener to h3 elements to invoke display function when clicked
+
+$("h3").click(display2);
